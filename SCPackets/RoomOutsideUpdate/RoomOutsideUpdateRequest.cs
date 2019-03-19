b5039baf9f7e68feb1183c0907922b825c3f7bb8
Crisey <kontogry96@gmail.com>
2019-03-19 10:21:21 +0100

    RoomOutsideUpdate after modify

---
+++ SCPackets/RoomOutsideUpdate/RoomOutsideUpdateRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Network.Packets;
+using SCPackets.Models;
+
+namespace SCPackets.RoomOutsideUpdate
+{
+    public class RoomOutsideUpdateRequest : RequestPacket
+    {
+        public RoomOutsideUpdateRequest(RoomOutsideModel roomOutsideModel)
+        {
+            RoomOutsideModel = roomOutsideModel;
+        }
+
+        public RoomOutsideModel RoomOutsideModel { get; set; }
+    }
+}
